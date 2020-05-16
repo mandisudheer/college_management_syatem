01f885581941d354b9b9ff519ed7b7aa1c6db3a8
mandisudheer <mandi.sudheer475@gmail.com>
2020-05-16 11:26:33 +0530

    project created

---
+++ project/application/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# custom user class
+class CustomUser(AbstractUser):
+    user_type_data=(
+        (1,"HOD"),
+        (2,"STAFF"),
+        (3,"STUDENT")
+    )
+    user_type=models.CharField(default=1,choices=user_type_data,max_length=30)
+class AdminHOD(models.Model):
+    id=models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+class Staffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=models.Manager()
+
+
+@receiver(post_save,sender=CustomUser)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        if instance.user_type==1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type==2:
+            Staffs.objects.create(admin=instance,address="")
+        if instance.user_type==3:
+            # Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2020-01-01",session_end_year="2021-01-01",address="",profile_pic="",gender="")
+            pass
+
+@receiver(post_save,sender=CustomUser)
+def save_user_profile(sender,instance,**kwargs):
+    if instance.user_type==1:
+        instance.adminhod.save()
+    if instance.user_type==2:
+        instance.staffs.save()
+    if instance.user_type==3:
+        instance.students.save()
